c7ed06bfce7d702bd26dc9a54e0f2aa7ee1e59f2
Kevin22-utn <kematamorosde@est.utn.ac.cr>
2026-08-21 22:07:17 -0600

    Se cambiaron lineas en clase main
    Por error se cambiaron lineas

--- src/proyecto_2/Proyecto_2.java
+++ src/proyecto_2/Proyecto_2.java
@@ -14,7 +14,7 @@ public class Proyecto_2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here 
     }
     
 }
