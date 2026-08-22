253bf86c33c0d0a0be0e1ba8b1f74103b39d881f
Kevin22-utn <kematamorosde@est.utn.ac.cr>
2026-08-21 22:19:59 -0600

    Se creo la clase ColaContratos
    Con sus metodos, utilizando estructuras dinamicas como Queue y LinkedList

---
+++ src/estructuras/ColaContratos.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package estructuras;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ *
+ * @author matam
+ */
+public class ColaContratos<T> {
+    private Queue<T> cola = new LinkedList<>();
+
+    public void encolar(T elemento) { 
+        cola.add(elemento); 
+    }
+    public T procesarSiguiente() { 
+        return cola.poll(); 
+    }
+    public T verPrimero() { 
+        return cola.peek(); 
+    }
+    public boolean estaVacia() { 
+        return cola.isEmpty(); 
+    }
+    public int getTamanio() { 
+        return cola.size(); 
+    }
+}
